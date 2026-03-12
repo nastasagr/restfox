e1ec78ca5e0c265629ba890a39cd1c684ddd0b72
nastasagr <alexanstgr@gmail.com>
2026-03-12 17:25:32 +0200

    fix text

--- core/traits/Permissions.php
+++ core/traits/Permissions.php
@@ -1,15 +1,14 @@
 <?php
 
-
 trait Permissions
 {
-    public function permissions_check(WP_REST_Request $request): WP_Error | Bool
+    public function permissions_check(WP_REST_Request $request): WP_Error|bool
     {
         $auth_header = $request->get_header('authorization');
 
         if (empty($auth_header)) {
             return new WP_Error(
-                'restman_no_auth_header',
+                'restfox_no_auth_header',
                 'Missing authorization header.',
                 ['status' => 401]
             );
@@ -17,7 +16,7 @@ trait Permissions
 
         if (stripos($auth_header, 'Bearer ') !== 0) {
             return new WP_Error(
-                'restman_bad_auth_header',
+                'restfox_bad_auth_header',
                 'Wrong authorization header (use Bearer token instead).',
                 ['status' => 401]
             );
@@ -27,35 +26,35 @@ trait Permissions
 
         if (empty($token)) {
             return new WP_Error(
-                'restman_admin_empty_token',
-                'restman token is empty',
+                'restfox_admin_empty_token',
+                'restfox token is empty',
                 ['status' => 401]
             );
         }
 
-        $user_id = get_transient($this->token_prefix . $token);
+        $user_id = get_transient($this->token_prefix.$token);
 
-        if (! $user_id) {
+        if (!$user_id) {
             return new WP_Error(
-                'restman_admin_invalid_token',
-                'restman token is invalid or expired',
+                'restfox_admin_invalid_token',
+                'restfox token is invalid or expired',
                 ['status' => 401]
             );
         }
 
         $user = get_user_by('id', $user_id);
 
-        if (! $user) {
+        if (!$user) {
             return new WP_Error(
-                'restman_user_not_found',
+                'restfox_user_not_found',
                 'Request user not exist',
                 ['status' => 401]
             );
         }
 
-        if (! user_can($user, 'manage_options')) {
+        if (!user_can($user, 'manage_options')) {
             return new WP_Error(
-                'restman_admin_user_not_allowed',
+                'restfox_admin_user_not_allowed',
                 'This user dont have administrator access',
                 ['status' => 403]
             );
